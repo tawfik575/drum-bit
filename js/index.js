22cf40966d67477975d915134d561b0933237f1a
tawfik575 <tawfikbsmrstucse@gmail.com>
2023-06-14 00:25:21 +0600

    fix audio playback issue

--- js/index.js
+++ js/index.js
@@ -15,25 +15,25 @@ function playSound(key) {
 
     switch (activeKey) {
         case "w":
-            audio = new Audio("../sounds/tom-1.mp3");
+            audio = new Audio("./sounds/tom-1.mp3");
             break;
         case "a":
-            audio = new Audio("../sounds/tom-2.mp3");
+            audio = new Audio("./sounds/tom-2.mp3");
             break;
         case "s":
-            audio = new Audio("../sounds/tom-3.mp3");
+            audio = new Audio("./sounds/tom-3.mp3");
             break;
         case "d":
-            audio = new Audio("../sounds/tom-4.mp3");
+            audio = new Audio("./sounds/tom-4.mp3");
             break;
         case "j":
-            audio = new Audio("../sounds/snare.mp3");
+            audio = new Audio("./sounds/snare.mp3");
             break;
         case "k":
-            audio = new Audio("../sounds/crash.mp3");
+            audio = new Audio("./sounds/crash.mp3");
             break;
         case "l":
-            audio = new Audio("../sounds/kick-bass.mp3");
+            audio = new Audio("./sounds/kick-bass.mp3");
             break;
         default:
             console.log(activeKey + " is not a valid key!");
